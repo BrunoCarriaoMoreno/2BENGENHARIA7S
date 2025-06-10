999881331f134e86c0e877cd1cfdcd3e70959680
Bruno <brunobatistacarriaomoreno@gmail.com>
2025-06-10 11:07:30 -0300

    configuração do swagger e .env de exemplo

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('CRUD JWT')
+    .setDescription('API com autenticação JWT')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log('aplicação rodando em http://localhost:3000');
 }
 bootstrap();
